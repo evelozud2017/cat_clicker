31e4b98b4d375ecc01cfaeb132939f1c37686cb4
Eva Veloz <eveloz@gmail.com>
2018-04-03 23:22:17 -0700

    5 cats with list

--- js/script.js
+++ js/script.js
@@ -8,31 +8,54 @@ var cat_data = [
     "name":"Chewie",
     "image":"img/chewie.jpg",
     "counter":0
+  },
+  {
+    "name":"Elise",
+    "image":"img/elise.jpg",
+    "counter":0
+  },
+  {
+    "name":"Rocket",
+    "image":"img/rocket.jpg",
+    "counter":0
+  },
+  {
+    "name":"Spud",
+    "image":"img/spud.jpg",
+    "counter":0
   }
 ];
 
-function displayRows() {
-  var htmlStr = "";
-  $.each(cat_data, function(idx, cat) {
-    htmlStr += "<div class='cat col-xs-12 col-sm-4 col-md-4' id='img'>";
-    htmlStr += "<img src='" + cat.image + "' class='clickable img-thumbnail'/>";
-    htmlStr += "<h4 class='name caption text-center'>" + cat.name + "</h4>";
-    htmlStr += "<p class='count caption text-center' id='" + idx.toString() + "'>" + cat.counter.toString() + "</p>";
-    htmlStr += "</div>";
+function createCatList() {
+  var toAppend = "";
+  $.each(cat_data, function(catIndex, cat) {
+    toAppend += "<li class='cat list-group-item'>" + cat.name + "</li>";
   });
-
-	$("#main").append("<div class='row'><div class='container'>" + htmlStr + "</div></div>");
+  $("#list").append("<ul class='list-group'>" + toAppend + "</ul>");
 }
 
-
-$(document).ready(function() {
-  //var counter = 0;
-  displayRows();
-  //$("#count").append("<p id='clicks'>" + counter.toString() + "</p>");
+function displayCat(id) {
+  $("#display").empty();
+  cat = cat_data[id];
+  var htmlStr =  "<div class='cat col-xs-12 col-sm-4 col-md-4' id='img'>";
+    htmlStr += "<img src='" + cat.image + "' class='clickable img-thumbnail'/>";
+    htmlStr += "<h4 class='name caption text-center'>" + cat.name + "</h4>";
+    htmlStr += "<p class='count caption text-center' id='" + id.toString() + "'>" + cat.counter.toString() + "</p>";
+    htmlStr += "</div>";
+  $("#display").append(htmlStr);
   $(".clickable").click(function(obj) {
     var elem = obj.target.parentElement.childNodes[2];
     cat_data[elem.id].counter += 1;
 
     $("#" + elem.id).text(cat_data[elem.id].counter);
+  });
+}
+
+
+$(document).ready(function() {
+  createCatList();
+  $(".cat").click(function(obj) {
+    id = cat_data.indexOf(cat_data.filter(function(a){ return a.name == obj.target.innerHTML; })[0]);
+    displayCat(id);
   });
 });
